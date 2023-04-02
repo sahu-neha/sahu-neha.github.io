8f86b1951da781d06a1bab6895894a9f917099b6
Neha <nehasahu2227@gmail.com>
2023-04-02 22:13:58 +0530

    draft

--- script/index.js
+++ script/index.js
@@ -1,22 +1,15 @@
-function typeWrite(e) {
-	var textoArray = e.innerHTML.split("");
-	e.innerHTML = " ";
-	textoArray.forEach(function (letter, i) {
-		setTimeout(function () {
-			e.innerHTML += letter;
-		}, 75 * i);
-	});
-}
-typeWrite(document.querySelector(".typewriter"));
+// function typeWrite(e) {
+// 	var textoArray = e.innerHTML.split("");
+// 	e.innerHTML = " ";
+// 	textoArray.forEach(function (letter, i) {
+// 		setTimeout(function () {
+// 			e.innerHTML += letter;
+// 		}, 75 * i);
+// 	});
+// }
+// typeWrite(document.querySelector(".typewriter"));
 
 // ---------------------------------//
-
-// var typed = new typed(".typewriter", {
-// 	strings: ["JAVA BACKEND DEVELOPER"],
-// 	typeSpeed: 75,
-// 	backSpeed: 75,
-// 	loop: true,
-// });
 
 //----------------------------------//
 
