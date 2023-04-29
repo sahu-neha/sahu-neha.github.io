ffc9c8e76935af39eea334e575e51ee7b5824d9c
Neha <nehasahu2227@gmail.com>
2023-04-29 16:31:07 +0530

    draft

--- script/index.js
+++ script/index.js
@@ -64,19 +64,4 @@ function menu() {
 }
 menu();
 
-// ==================== DISABLE RIGHT CLICK ==================== //
-
-var message = "Something went wrong";
-function rtclickcheck(keyp) {
-	if (navigator.appName == "Netscape" && keyp.which == 3) {
-		alert(message);
-		return false;
-	}
-	if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) {
-		alert(message);
-		return false;
-	}
-}
-document.onmousedown = rtclickcheck;
-
 //----------------------------------//
